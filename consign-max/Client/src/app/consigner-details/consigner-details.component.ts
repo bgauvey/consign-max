66551fa52679577ba0821a3aa1561592779b31ea
Bill Gauvey <bgauvey@gmail.vom>
2019-09-05 19:56:41 -0400

    added consignor pages

---
+++ consign-max/Client/src/app/consigner-details/consigner-details.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-consigner-details',
+  templateUrl: './consigner-details.component.html',
+  styleUrls: ['./consigner-details.component.css']
+})
+export class ConsignerDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
