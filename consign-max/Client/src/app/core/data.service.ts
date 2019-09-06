bc37fc39ab095351782fdb024d3363bf41a83b4f
Bill Gauvey <bgauvey@gmail.vom>
2019-09-06 05:44:36 -0400

    added paging

---
+++ consign-max/Client/src/app/core/data.service.ts
@@ -0,0 +1,150 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { StateDebouncer } from '@clr/angular/data/datagrid/providers/state-debouncer.provider';
+import { Observable } from 'rxjs';
+import { map, catchError } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+  baseUrl = '/api/';
+  baseConsignersUrl = this.baseUrl + 'consignors';
+  baseStatesUrl = this.baseUrl + 'states';
+
+  constructor(private http: HttpClient) {}
+
+  getConsigners(): Observable<IConsigner[]>  {
+    return this.http.get<IConsigner[]>(this.baseConsignersUrl)
+    .pipe(
+           map(consigners => {
+               this.calculateConsignersAmountDue(consigners);
+               return consigners;
+           }),
+           catchError(this.handleError)
+        );
+  }
+
+  getConsignersPage(page: number, pageSize: number) : Observable<IPagedResults<IConsigner[]>> {
+    return this.http.get<IConsigner[]>(`${this.baseConsignersUrl}/page/${page}/${pageSize}`, {observe: 'response'})
+        .pipe(
+            map((res) => {
+                //Need to observe response in order to get to this header (see {observe: 'response'} above)
+                const totalRecords = +res.headers.get('x-inlinecount');
+                let consigners = res.body as IConsigner[];
+                this.calculateConsignersAmountDue(consigners);
+                return {
+                    results: consigners,
+                    totalRecords: totalRecords
+                };
+            }),
+            catchError(this.handleError)
+        );
+  }
+
+  getConsigner(id: string) : Observable<IConsigner> {
+    return this.http.get<IConsigner>(this.baseConsignersUrl + '/' + id)
+        .pipe(
+            catchError(this.handleError)
+        );
+}
+
+insertConsigner(consigner: IConsigner) : Observable<IConsigner> {
+    return this.http.post<IConsignerResponse>(this.baseConsignersUrl, consigner)
+        .pipe(
+            map((data) => {
+                   console.log('insertConsigner status: ' + data.status);
+                   return data.consigner;
+               }),
+            catchError(this.handleError)
+        );
+}
+
+updateConsigner(consigner: IConsigner) : Observable<IConsigner> {
+    return this.http.put<IConsignerResponse>(this.baseConsignersUrl + '/' + consigner.id, consigner)
+        .pipe(
+            map((data) => {
+                   console.log('updateConsigner status: ' + data.status);
+                   return data.consigner;
+               }),
+            catchError(this.handleError)
+        );
+}
+
+deleteConsigner(id: string) : Observable<boolean> {
+    return this.http.delete<boolean>(this.baseConsignersUrl + '/' + id)
+        .pipe(
+            catchError(this.handleError)
+        );
+}
+
+  calculateConsignersAmountDue(consigners: IConsigner[]) {
+    for (let consigner of consigners) {
+        if (consigner && consigner.items) {
+            let total = 0;
+            for (let item of consigner.items) {
+                total += item.salePrice;
+            }
+            consigner.amountDue = total;
+        }
+    }
+  }
+
+  getStates(): Observable<IState[]> {
+    return this.http.get<IState[]>(this.baseStatesUrl)
+        .pipe(
+            catchError(this.handleError)
+        );
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    console.error('server error:', error);
+    if (error.error instanceof Error) {
+      let errMessage = error.error.message;
+      return Observable.throw(errMessage);
+    }
+    return Observable.throw(error || 'ASP.NET Core server error');
+  }
+}
+
+export interface IConsigner {
+  id: number;
+  lastName: string;
+  middleInitial: string;
+  firstName: string;
+  addressLine1: string;
+  addressLine2: string;
+  city: string;
+  state: IState;
+  zip: string;
+  mobilePhone: string;
+  phone: string;
+  emailAddress: string;
+  commission: number;
+  updateDate: Date;
+  createDate: Date;
+  items: IItem[];
+  amountDue: number
+}
+
+export interface IState {
+  id: number;
+  abbreviation: string;
+  name: string;
+}
+
+export interface IItem {
+  id: number;
+  consignorId: number;
+  description: string;
+  askingPrice: number;
+  minimumPrice: number;
+  endingPrice: number;
+  commission: number;
+  listingFee: number;
+  saleDayLimit: number;
+  saleDate: Date;
+  salePrice: number;
+  updateDate: Date;
+  createDate: Date;
+}
